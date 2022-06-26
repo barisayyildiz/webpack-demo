76a718b6b593cfd8518768d1a275bf8fe0c2000b
barisayyildiz <barisayyildiz.1799@gmail.com>
2022-06-26 18:13:26 +0300

    initial commit

---
+++ src/script.js
@@ -0,0 +1,9 @@
+import './style.scss'
+import moment from "moment"
+
+console.log("hello from javascript")
+console.log(moment().startOf("day").fromNow())
+
+var name = "Bob", time = "today";  
+console.log(`Hello ${name}, how are you ${time}?`);
+
